fbf3a0de55e4dce51614fa6c1497ccfd60772145
Alberto Rocha <betogrun@gmail.com>
2017-09-08 01:03:30 -0300

    Update prod config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   #  config.action_mailer.default_url_options = { :host => request.host_with_port }
-    config.action_mailer.default_url_options = { :host => 'http://localhost:3000' }
+    config.action_mailer.default_url_options = { :host => '104.236.55.161' }
     config.action_mailer.delivery_method = :smtp
 
     config.action_mailer.raise_delivery_errors = true
